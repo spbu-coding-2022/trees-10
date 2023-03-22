6825a6fb2b4edbd28318142f6f878d5a86f18ed9
d-zaytsev <dzaytsev308@gmail.com>
2023-03-22 23:04:03 +0300

    feat: Add a search fun in BinaryNode

---
+++ src/nodes/BinaryNode/BinaryNode.kt
@@ -0,0 +1,27 @@
+package BinaryNode
+
+class BinaryNode<T: Comparable<T>, NodeType>(key : T, value : NodeType) : Comparable<BinaryNode<T, NodeType>> {
+
+    var key : T
+    var value : NodeType
+
+    private var right : BinaryNode<T, NodeType>? = null
+    private var left : BinaryNode<T, NodeType>? = null
+
+    init {
+        key.also { this.key = it }
+        value.also { this.value = it }
+    }
+
+    fun search(key: T) : NodeType? =
+        when (this.compareTo(key)) {
+            1 -> this.right?.search(key)
+            0 -> this.value
+            -1 -> this.left?.search(key)
+            else -> null
+        }
+
+
+    override fun compareTo(other: BinaryNode<T, NodeType>): Int = this.key.compareTo(other.key)
+    fun compareTo(key: T): Int = this.key.compareTo(key)
+}
