7388c74742c90655369c3ee543e4e72ed15427fb
d-zaytsev <dzaytsev308@gmail.com>
2023-04-16 23:50:59 +0300

    Реализовал интерфейс в BinaryNode

--- lib/src/main/kotlin/trees/BinaryTree.kt
+++ lib/src/main/kotlin/trees/BinaryTree.kt
@@ -1,14 +1,14 @@
 package trees
 
-import BinaryNode.BinaryNode
+import nodes.BinaryNode
 
-open class BinaryTree<T: Comparable<T>, NodeType> {
+open class BinaryTree<T: Comparable<T>, NodeType> : ITree<T, NodeType> {
     protected open var root : BinaryNode<T, NodeType>? = null
-    open fun search(key : T) : NodeType? = root?.search(key)
-    open fun remove(key: T)  {
+    override fun search(key : T) : NodeType? = root?.search(key)
+    override fun remove(key: T)  {
         root = root?.remove(this.root, key)
     }
-    open fun add(key : T, value : NodeType? = null) {
+    override fun add(key : T, value : NodeType?) {
         if (root == null)
             root = BinaryNode(key, value)
         else {
