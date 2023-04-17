60312f04369d98e94150c37d402fc49399813e8f
Ilya Pogorelov <ilya_pogorelov_04@mail.ru>
2023-04-18 02:12:19 +0300

    Add RBTree Node class

---
+++ lib/src/main/kotlin/nodes/RBTNode.kt
@@ -0,0 +1,21 @@
+import nodes
+
+package RBTNode
+
+class RBNode<K : Comparable<K>, V>(override var key: K, override var value: V?) :
+    AbstractNode<K, V, RBNode<K, V>>() {
+
+    override var right: RBNode<K, V>? = null
+    override var left: RBNode<K, V>? = null
+    var color: Color = Color.BLACK
+        private set
+
+    fun flipColor() {
+        color = when (color) {
+            Color.RED -> Color.BLACK
+            Color.BLACK -> Color.RED
+        }
+    }
+}
+
+enum class Color { RED, BLACK }
