6e0fa9c92c0fc1ee00d447c88f207e78929dd3af
d-zaytsev <dzaytsev308@gmail.com>
2023-04-20 22:53:27 +0300

    Добавлено новое исключение NullNodeException

--- lib/src/main/kotlin/exceptions/TreeExceptions.kt
+++ lib/src/main/kotlin/exceptions/TreeExceptions.kt
@@ -8,3 +8,5 @@ open class TreeException(message: String) : Exception(message)
 class NodeNotFoundException() : TreeException("Подходящий для выполнения операции узел дерева не найден")
 
 class NodeAlreadyExistsException() : TreeException("Узел дерева с таким ключом уже существует")
+
+class NullNodeException() : TreeException("Узел дерева принял значение null")
