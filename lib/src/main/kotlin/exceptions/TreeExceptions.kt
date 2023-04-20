ee1077de587f7d1f4f456c4511dabf39e81c9db7
d-zaytsev <dzaytsev308@gmail.com>
2023-04-20 18:29:22 +0300

    Добавлены два класса (NodeNotFound, NodeAlreadyExists) для выбрасывания соотвествующих исключений

--- lib/src/main/kotlin/exceptions/TreeExceptions.kt
+++ lib/src/main/kotlin/exceptions/TreeExceptions.kt
@@ -3,4 +3,8 @@ package exceptions
 /**
  * Общий класс для всех исключений, связанных с деревьями
  * */
-open class TreeException(message: String, cause: Throwable? = null) : Exception(message, cause)
+open class TreeException(message: String) : Exception(message)
+
+class NodeNotFound() : TreeException("Подходящий узел дерева не найден")
+
+class NodeAlreadyExists() : TreeException("Такой узел дерева уже существует")
