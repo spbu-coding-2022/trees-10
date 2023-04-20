6ec2e7525016b03bf84dc53a2da2d04b1330bdc8
d-zaytsev <dzaytsev308@gmail.com>
2023-04-20 18:42:30 +0300

    При попытке удаления несуществующей ноды выбрасывается исключение

--- lib/src/main/kotlin/nodes/BinaryNode.kt
+++ lib/src/main/kotlin/nodes/BinaryNode.kt
@@ -1,5 +1,7 @@
 package nodes
 
+import exceptions.NodeNotFound
+
 /**
  * Класс узла для бинарного дерева.
  *
@@ -9,50 +11,53 @@ package nodes
  * @property value значение узла.
  * @author Dmitriy Zaytsev
  */
-open class BinaryNode<K: Comparable<K>, V>(override var key: K, override var value: V?)  : AbstractNode<K, V, BinaryNode<K, V>>() {
+open class BinaryNode<K : Comparable<K>, V>(override var key: K, override var value: V?) :
+    AbstractNode<K, V, BinaryNode<K, V>>() {
 
-    override var right : BinaryNode<K, V>? = null
-    override var left : BinaryNode<K, V>? = null
+    override var right: BinaryNode<K, V>? = null
+    override var left: BinaryNode<K, V>? = null
 
-    open fun search(key: K) : BinaryNode<K, V>? =
+    open fun search(key: K): BinaryNode<K, V>? =
         when (key.compareTo(this.key)) {
             1 -> this.right?.search(key)
             0 -> this
             -1 -> this.left?.search(key)
             else -> null
-    }
+        }
 
-    open fun remove(root : BinaryNode<K, V>?, key : K) : BinaryNode<K, V>? {
-        if (root == null)
-            return null
+    open fun remove(root: BinaryNode<K, V>?, key: K): BinaryNode<K, V>? {
         if (key == this.key) { // когда remove вызывается для удаляемой вершины
-            if (this.right == null && this.left == null)
-                return null
+            if (right == null && left == null)
+                return null // просто стираем ноду
+
             // Простой случай - если есть только 1 потомок
-            else if  (this.left == null)
-                return this.right
-            else if (this.right == null)
-                return this.left
-            // Есть оба поддерева
+            else if (left == null)
+                return right
+            else if (right == null)
+                return left
+
+            // Случай где есть два потомка
             else {
                 // Находим минимальное дерево
+                val minNode = findMin(right)
                 // Перенимаем его key и value
-                // Удаляем минимальное дерево
-                val minNode = findMin(this.right)
                 minNode?.let {
                     this.key = it.key
                     this.value = it.value
                 }
+                // Удаляем минимальное дерево
                 this.right = right?.remove(right, key)
                 return this
             }
 
         } else {
-            // Замещаем поддеревья на минимальные
+            // Идём дальше по дереву искать что удалить
             if (key < this.key)
                 this.left = left?.remove(left, key)
+                    ?: throw NodeNotFound()
             else
                 this.right = right?.remove(right, key)
+                    ?: throw NodeNotFound()
             return root
         }
     }
@@ -61,9 +66,14 @@ open class BinaryNode<K: Comparable<K>, V>(override var key: K, override var val
      * @param[node] Узел для которого ищется минимальный эл-т.
      * @return Наименьший узел.
      */
-    private fun findMin(node: BinaryNode<K, V>?): BinaryNode<K, V>? = if (node?.left != null) findMin(node.left) else node
+    private fun findMin(node: BinaryNode<K, V>?): BinaryNode<K, V>? {
+        return if (node?.left != null)
+            findMin(node.left)
+        else
+            node
+    }
 
-    open fun add(key : K, value : V?) {
+    open fun add(key: K, value: V?) {
         val compare = key.compareTo(this.key)
 
         if (compare == 1) {
--- lib/src/main/kotlin/trees/BinaryTree.kt
+++ lib/src/main/kotlin/trees/BinaryTree.kt
@@ -1,11 +1,14 @@
 package trees
 
+import exceptions.NodeNotFound
 import nodes.BinaryNode
 
 open class BinaryTree<K: Comparable<K>, V> : AbstractTree<K, V, BinaryNode<K,V>>() {
     override var root : BinaryNode<K, V>? = null
     override fun search(key : K) : BinaryNode<K,V>? = root?.search(key)
     override fun remove(key: K)  {
+        if (root == null)
+            throw NodeNotFound()
         root = root?.remove(this.root, key)
     }
     override fun add(key : K, value : V?) {
