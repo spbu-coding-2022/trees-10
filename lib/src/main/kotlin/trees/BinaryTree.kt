eb3d49fd1034d931c6b424457af80e19da8fb491
d-zaytsev <dzaytsev308@gmail.com>
2023-04-20 18:48:37 +0300

    При поиске несуществующей ноды выбрасывается исключение

--- lib/src/main/kotlin/trees/BinaryTree.kt
+++ lib/src/main/kotlin/trees/BinaryTree.kt
@@ -6,10 +6,9 @@ import nodes.BinaryNode
 open class BinaryTree<K: Comparable<K>, V> : AbstractTree<K, V, BinaryNode<K,V>>() {
     override var root : BinaryNode<K, V>? = null
     override fun search(key : K) : BinaryNode<K,V>? = root?.search(key)
+        ?: throw NodeNotFound()
     override fun remove(key: K)  {
-        if (root == null)
-            throw NodeNotFound()
-        root = root?.remove(this.root, key)
+        root = root?.remove(this.root, key) ?: throw NodeNotFound()
     }
     override fun add(key : K, value : V?) {
         if (root == null)
