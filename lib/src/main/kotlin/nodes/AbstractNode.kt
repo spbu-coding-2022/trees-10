b85198a52762aee4fe3636361d3eb8770b2a7fff
d-zaytsev <dzaytsev308@gmail.com>
2023-04-20 17:56:34 +0300

    Тип передаваемой в абстрактные классы ноды теперь просто node, а не NodeType

--- lib/src/main/kotlin/nodes/AbstractNode.kt
+++ lib/src/main/kotlin/nodes/AbstractNode.kt
@@ -1,11 +1,11 @@
 package nodes
 
-abstract class AbstractNode<K : Comparable<K>, V, NodeType : AbstractNode<K, V, NodeType>>() {
+abstract class AbstractNode<K : Comparable<K>, V, node : AbstractNode<K, V, node>>() {
     abstract var key : K
         protected set
     abstract var value : V?
         protected set
 
-    abstract var right : NodeType?
-    abstract var left : NodeType?
+    abstract var right : node?
+    abstract var left : node?
 }
