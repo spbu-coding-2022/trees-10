fc50363c82403ce9673bee819e8c91e34f8c4ecd
d-zaytsev <dzaytsev308@gmail.com>
2023-04-20 20:16:25 +0300

    Ноды теперь можно сравнивать между собой (по key)

--- lib/src/main/kotlin/nodes/AbstractNode.kt
+++ lib/src/main/kotlin/nodes/AbstractNode.kt
@@ -1,11 +1,14 @@
 package nodes
 
-abstract class AbstractNode<K : Comparable<K>, V, node : AbstractNode<K, V, node>>(key: K, value : V?) {
-    var key : K = key
+abstract class AbstractNode<K : Comparable<K>, V, node : AbstractNode<K, V, node>>(key: K, value: V?) :
+    Comparable<AbstractNode<K, V, node>> {
+    var key: K = key
         protected set
-    var value : V? = value
-     protected set
+    var value: V? = value
+        protected set
 
-    var right : node? = null
-    var left : node? = null
+    var right: node? = null
+    var left: node? = null
+
+    override fun compareTo(other: AbstractNode<K, V, node>): Int = this.key.compareTo(other.key)
 }
