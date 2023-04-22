ab50d0eabd7318ecdee0f695d483f333f186a629
d-zaytsev <dzaytsev308@gmail.com>
2023-04-22 20:18:19 +0300

    Теперь для создания класса WrappedBinTree необходимо передать ему бинарное дерево. Восстановить дерево можно обратившись к свойству binaryTree

--- lib/src/main/kotlin/databases/binTree/WrappedBinTree.kt
+++ lib/src/main/kotlin/databases/binTree/WrappedBinTree.kt
@@ -3,27 +3,30 @@ package databases.binTree
 import exceptions.NodeNotFoundException
 import exceptions.NullNodeException
 import nodes.BinaryNode
-import trees.AbstractTree
 import trees.BinaryTree
 
 data class Node<NodeType>(val binNode: NodeType, val x: Double, val y: Double)
 
-class WrappedBinTree<K : Comparable<K>, V>() {
+class WrappedBinTree<K : Comparable<K>, V>(tree: BinaryTree<K, V>) {
 
+    // Добавление бинарного деерва
+    init {
+        addNode(tree.root)
+    }
+
+    // Список с расширенными нодами
     private var wrappedNodesList: MutableList<WrappedBinNode<K, V>> = mutableListOf()
-    fun addTree(tree: BinaryTree<K, V>) {
-        if (tree.root == null)
-            return
 
-        addNode(tree.root ?: throw NullNodeException())
-    }
-
-    fun getTree(): BinaryTree<K, V> {
-        val resBinTree = BinaryTree<K, V>()
-        for (item in wrappedNodesList)
-            resBinTree.add(item.key, item.value)
-        return resBinTree
-    }
+    /**
+     * Позволяет получить бинарное дерево
+     */
+    val binaryTree: BinaryTree<K,V>
+        get() {
+            val resBinTree = BinaryTree<K, V>()
+            for (item in wrappedNodesList)
+                resBinTree.add(item.key, item.value)
+            return resBinTree
+        }
 
     fun setCoordinate(key: K, x: Double, y: Double) {
         for (item in wrappedNodesList) {
@@ -37,6 +40,7 @@ class WrappedBinTree<K : Comparable<K>, V>() {
         // Если ключ не совпадает ни с одной вершиной из имеющихся
         throw NodeNotFoundException()
     }
+
     fun getWrappedNode(key: K): WrappedBinNode<K, V> {
         for (item in wrappedNodesList)
             if (item.key == key)
@@ -46,11 +50,14 @@ class WrappedBinTree<K : Comparable<K>, V>() {
         throw NodeNotFoundException()
     }
 
-    private fun addNode(node: BinaryNode<K, V>) {
+    private fun addNode(node: BinaryNode<K, V>?) {
+        if (node == null)
+            return
         this.wrappedNodesList.add(WrappedBinNode(node.key, node.value))
         if (node.left != null)
             addNode(node.left ?: throw NullNodeException())
         if (node.right != null)
             addNode(node.right ?: throw NullNodeException())
     }
+
 }
