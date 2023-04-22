b51f99ad82a8bed378c208e1599d02990d35c203
d-zaytsev <dzaytsev308@gmail.com>
2023-04-22 14:39:27 +0300

    Добавлена возможность менять координаты у нод в WrappedBinTree

--- lib/src/main/kotlin/databases/binTree/WrappedBinTree.kt
+++ lib/src/main/kotlin/databases/binTree/WrappedBinTree.kt
@@ -1,5 +1,6 @@
 package databases.binTree
 
+import exceptions.NodeNotFoundException
 import exceptions.NullNodeException
 import nodes.BinaryNode
 import trees.AbstractTree
@@ -9,8 +10,7 @@ data class Node<NodeType>(val binNode: NodeType, val x: Double, val y: Double)
 
 class WrappedBinTree<K : Comparable<K>, V>() {
 
-    private var wrappedNodesList : MutableList<WrappedBinNode<K, V>> = mutableListOf()
-
+    private var wrappedNodesList: MutableList<WrappedBinNode<K, V>> = mutableListOf()
     fun addTree(tree: BinaryTree<K, V>) {
         if (tree.root == null)
             return
@@ -18,11 +18,33 @@ class WrappedBinTree<K : Comparable<K>, V>() {
         addNode(tree.root ?: throw NullNodeException())
     }
 
-    fun getTree() : BinaryTree<K, V> {
+    fun getTree(): BinaryTree<K, V> {
         val resBinTree = BinaryTree<K, V>()
         for (item in wrappedNodesList)
             resBinTree.add(item.key, item.value)
         return resBinTree
+    }
+
+    fun setCoordinate(key: K, x: Double, y: Double) {
+        for (item in wrappedNodesList) {
+            if (item.key == key) {
+                item.x = x
+                item.y = y
+                return
+            }
+        }
+
+        // Если ключ не совпадает ни с одной вершиной из имеющихся
+        throw NodeNotFoundException()
+    }
+
+    fun getWrappedNode(key: K): WrappedBinNode<K, V> {
+        for (item in wrappedNodesList)
+            if (item.key == key)
+                return item
+
+        // Если ключ не совпадает ни с одной вершиной из имеющихся
+        throw NodeNotFoundException()
     }
 
     private fun addNode(node: BinaryNode<K, V>) {
