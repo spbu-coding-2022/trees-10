037ee3857c363c45809b3e913355125d8488be72
d-zaytsev <dzaytsev308@gmail.com>
2023-04-23 22:06:14 +0300

    Класс BinTreeBase обновлён с учётом новой архитектуры

--- lib/src/main/kotlin/databases/binTree/BinTreeBase.kt
+++ lib/src/main/kotlin/databases/binTree/BinTreeBase.kt
@@ -1,54 +1,49 @@
-//package databases.binTree
-//
-//import nodes.BinaryNode
-//import trees.BinaryTree
-//import java.io.Closeable
-//import java.sql.DriverManager
-//import java.sql.SQLException
-//class BinaryBase (dbPath: String): Closeable {
-//
-//    private val connection = DriverManager.getConnection("jdbc:sqlite:$dbPath")
-//        ?: throw SQLException("Cannot connect to database")
-//    private val createBaseStatement by lazy {connection.prepareStatement("CREATE TABLE if not exists BinaryNodes (x int, y int, key int, value varchar(255));")}
-//    private val addNodeStatement by lazy { connection.prepareStatement("INSERT INTO BinaryNodes (x, y, key, value) VALUES (?, ?, ?, ?);") }
-//    private val getNodeByKey by lazy { connection.prepareStatement("SELECT x, y, value FROM BinaryNodes WHERE BinaryNodes.key = ?;") }
-//    private val removeNodeByKey by lazy { connection.prepareStatement("DELETE FROM BinaryNodes WHERE key=?;") }
-//    fun open() = createBaseStatement.execute()
-//
-//    fun saveBinaryTree(tree: BinaryTree<Int, String>) {
-////        if (tree.root)ы
-//    }
-//
-//    fun removeNode(key : Int) {
-//        removeNodeByKey.setInt(1, key)
-//        removeNodeByKey.execute()
-//    }
-//    fun addNode(node : Node<BinaryNode<Int, String>>) {
-//        if (search(node.binNode.key) != null)
-//            return
-//
-//        addNodeStatement.setInt(1, node.x)
-//        addNodeStatement.setInt(2, node.y)
-//        addNodeStatement.setInt(3, node.binNode.key)
-//        addNodeStatement.setString(4, node.binNode.value ?: "empty")
-//
-//        addNodeStatement.execute()
-//    }
-//    fun search(key: Int) : Node<BinaryNode<Int, String>>? {
-//        getNodeByKey.setInt(1, key)
-//        val res = getNodeByKey.executeQuery()
-//        if (res.isClosed)
-//            return null
-//
-//        return Node(BinaryNode(key, res.getString("value")), res.getInt("x"), res.getInt("y"))
-//    }
-//    override fun close() {
-//        createBaseStatement.close()
-//        addNodeStatement.close()
-//        getNodeByKey.close()
-//        removeNodeByKey.close()
-//
-//        connection.close()
-//    }
-//
-//}
+package databases.binTree
+
+import nodes.BinaryNode
+import trees.BinaryTree
+import java.io.Closeable
+import java.sql.DriverManager
+import java.sql.SQLException
+class BinaryBase (dbPath: String): Closeable {
+
+    private val connection = DriverManager.getConnection("jdbc:sqlite:$dbPath")
+        ?: throw SQLException("Cannot connect to database")
+    private val createBaseStatement by lazy {connection.prepareStatement("CREATE TABLE if not exists BinaryNodes (key int, value varchar(255), x double, y double);")}
+    private val addNodeStatement by lazy { connection.prepareStatement("INSERT INTO BinaryNodes (key, value, x, y) VALUES (?, ?, ?, ?);") }
+    private val getNodeByKey by lazy { connection.prepareStatement("SELECT x, y, value FROM BinaryNodes WHERE BinaryNodes.key = ?;") }
+    private val removeNodeByKey by lazy { connection.prepareStatement("DELETE FROM BinaryNodes WHERE key=?;") }
+    fun open() = createBaseStatement.execute()
+    fun removeNode(key : Int) {
+        removeNodeByKey.setInt(1, key)
+        removeNodeByKey.execute()
+    }
+    fun addNode(node : WrappedBinNode<Int, String>) {
+        if (search(node.key) != null)
+            return
+
+        addNodeStatement.setInt(1, node.key)
+        addNodeStatement.setString(2, node.value ?: "empty")
+        addNodeStatement.setDouble(3, node.x)
+        addNodeStatement.setDouble(4, node.y)
+
+        addNodeStatement.execute()
+    }
+    fun search(key: Int) : WrappedBinNode<Int, String>? {
+        getNodeByKey.setInt(1, key)
+        val res = getNodeByKey.executeQuery()
+        if (res.isClosed)
+            return null
+
+        return WrappedBinNode(key, res.getString("value"), res.getDouble("x"), res.getDouble("y"))
+    }
+    override fun close() {
+        createBaseStatement.close()
+        addNodeStatement.close()
+        getNodeByKey.close()
+        removeNodeByKey.close()
+
+        connection.close()
+    }
+
+}
