246a1f94a3a4029f496fc16f202feb90092c83eb
d-zaytsev <dzaytsev308@gmail.com>
2023-04-23 22:33:51 +0300

    Реализовал возможность доставать бинарное дерево поиска из базы данных

--- lib/src/main/kotlin/databases/binTree/BinTreeBase.kt
+++ lib/src/main/kotlin/databases/binTree/BinTreeBase.kt
@@ -1,33 +1,50 @@
 package databases.binTree
 
-import nodes.BinaryNode
 import trees.BinaryTree
 import java.io.Closeable
 import java.sql.DriverManager
 import java.sql.SQLException
-class BinaryBase (dbPath: String): Closeable {
+
+class BinaryBase(dbPath: String) : Closeable {
 
     private val connection = DriverManager.getConnection("jdbc:sqlite:$dbPath")
         ?: throw SQLException("Cannot connect to database")
-    private val createBaseStatement by lazy {connection.prepareStatement("CREATE TABLE if not exists BinaryNodes (key int, value varchar(255), x double, y double);")}
+    private val createBaseStatement by lazy { connection.prepareStatement("CREATE TABLE if not exists BinaryNodes (key int, value varchar(255), x double, y double);") }
     private val addNodeStatement by lazy { connection.prepareStatement("INSERT INTO BinaryNodes (key, value, x, y) VALUES (?, ?, ?, ?);") }
-    private val getNodeByKey by lazy { connection.prepareStatement("SELECT x, y, value FROM BinaryNodes WHERE BinaryNodes.key = ?;") }
-    private val removeNodeByKey by lazy { connection.prepareStatement("DELETE FROM BinaryNodes WHERE key=?;") }
+    private val getNodesStatement by lazy { connection.prepareStatement("SELECT key, value, x, y, value FROM BinaryNodes") }
     fun open() = createBaseStatement.execute()
 
     fun saveTree(tree: WrappedBinTree<Int, String>) {
         for (node in tree.getWrappedNodesArray())
             addNode(node)
     }
-    fun saveTree(tree: BinaryTree<Int, String>) = saveTree(WrappedBinTree(tree))
-    fun removeNode(key : Int) {
-        removeNodeByKey.setInt(1, key)
-        removeNodeByKey.execute()
-    }
-    private fun addNode(node : WrappedBinNode<Int, String>) {
-        if (search(node.key) != null)
-            return
 
+    fun saveTree(tree: BinaryTree<Int, String>) = saveTree(WrappedBinTree(tree))
+    fun getWrappedBinTree(): WrappedBinTree<Int, String> {
+        val tree = WrappedBinTree<Int, String>()
+
+        val stateRes = getNodesStatement.executeQuery()
+
+        while (!stateRes.isClosed && stateRes.next()) {
+            tree.add(stateRes.getInt(1), stateRes.getString(2), stateRes.getDouble(3), stateRes.getDouble(4))
+        }
+
+        return tree
+    }
+
+    fun getBinaryTree(): BinaryTree<Int, String> {
+        val tree = BinaryTree<Int, String>()
+
+        val stateRes = getNodesStatement.executeQuery()
+
+        while (!stateRes.isClosed && stateRes.next()) {
+            tree.add(stateRes.getInt(1), stateRes.getString(2))
+        }
+
+        return tree
+    }
+
+    private fun addNode(node: WrappedBinNode<Int, String>) {
         addNodeStatement.setInt(1, node.key)
         addNodeStatement.setString(2, node.value ?: "empty")
         addNodeStatement.setDouble(3, node.x)
@@ -35,25 +52,18 @@ class BinaryBase (dbPath: String): Closeable {
 
         addNodeStatement.execute()
     }
-    fun search(key: Int) : WrappedBinNode<Int, String>? {
-        getNodeByKey.setInt(1, key)
-        val res = getNodeByKey.executeQuery()
-        if (res.isClosed)
-            return null
 
-        return WrappedBinNode(key, res.getString("value"), res.getDouble("x"), res.getDouble("y"))
-    }
     constructor(dbPath: String, tree: WrappedBinTree<Int, String>) : this(dbPath) {
         saveTree(tree)
     }
+
     constructor(dbPath: String, tree: BinaryTree<Int, String>) : this(dbPath) {
         saveTree(tree)
     }
+
     override fun close() {
         createBaseStatement.close()
         addNodeStatement.close()
-        getNodeByKey.close()
-        removeNodeByKey.close()
 
         connection.close()
     }
