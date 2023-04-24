b9d1d6b17a2cf99129856807f71f88beea55f810
d-zaytsev <dzaytsev308@gmail.com>
2023-04-24 10:14:27 +0300

    Убрал возможность сохранять не WrappedBinTree

--- lib/src/main/kotlin/databases/binTree/BinTreeBase.kt
+++ lib/src/main/kotlin/databases/binTree/BinTreeBase.kt
@@ -6,9 +6,9 @@ import java.sql.DriverManager
 import java.sql.SQLException
 
 
-class BinTreeBase <V>(
+class BinTreeBase<V>(
     dbPath: String,
-    private val serializeValue: (value: V?) -> String,
+    private val serializeValue: (value: V?) -> String = { value -> value.toString() },
     private val deserializeValue: (strValue: String) -> V
 ) : Closeable {
 
@@ -17,22 +17,27 @@ class BinTreeBase <V>(
     private val createBaseStatement by lazy { connection.prepareStatement("CREATE TABLE if not exists BinaryNodes (key int, value varchar(255), x double, y double);") }
     private val addNodeStatement by lazy { connection.prepareStatement("INSERT INTO BinaryNodes (key, value, x, y) VALUES (?, ?, ?, ?);") }
     private val getNodesStatement by lazy { connection.prepareStatement("SELECT key, value, x, y, value FROM BinaryNodes") }
+
     init {
         createBaseStatement.execute()
     }
+
     fun saveTree(tree: WrappedBinTree<Int, V>) {
         for (node in tree.getWrappedNodesArray())
             addNode(node)
     }
-
-    fun saveTree(tree: BinaryTree<Int, V>) = saveTree(WrappedBinTree(tree))
     fun getWrappedBinTree(): WrappedBinTree<Int, V> {
         val tree = WrappedBinTree<Int, V>()
 
         val stateRes = getNodesStatement.executeQuery()
 
         while (!stateRes.isClosed && stateRes.next()) {
-            tree.add(stateRes.getInt(1), deserializeValue(stateRes.getString(2)), stateRes.getDouble(3), stateRes.getDouble(4))
+            tree.add(
+                stateRes.getInt(1),
+                deserializeValue(stateRes.getString(2)),
+                stateRes.getDouble(3),
+                stateRes.getDouble(4)
+            )
         }
 
         return tree
