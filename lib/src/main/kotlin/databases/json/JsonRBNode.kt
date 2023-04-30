7910c68e910be2fdf78d91da38aebd5a400bef28
d-zaytsev <dzaytsev308@gmail.com>
2023-04-30 20:35:13 +0300

    Добавил хранение цвета в JsonRBNode

--- lib/src/main/kotlin/databases/json/JsonRBNode.kt
+++ lib/src/main/kotlin/databases/json/JsonRBNode.kt
@@ -1,11 +1,13 @@
 package databases.json
 
 import kotlinx.serialization.Serializable
+import nodes.Color
 
 @Serializable
 data class JsonRBNode(
     val value: String,
     val key: Int,
+    val color: Color,
     val left: JsonRBNode?,
     val right: JsonRBNode?
 )
